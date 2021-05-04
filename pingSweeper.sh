016ac50bd25c62f0715b8a29412464616a67fb25
purpler0se <78451563+purpler0se@users.noreply.github.com>
2021-05-03 20:42:43 -0400

    Just added colour ;-;

--- pingSweeper.sh
+++ pingSweeper.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-if [ $1 == "" ];
+RED='\033[0;31m'
+if [ "$1" == "" ];
 then
-echo "You forgot to enter the IP address you wanted to sweep provide the first three octates you want to scan"
-echo "Syntax ./scriptname.sh 192.18.1."
+        echo "You forgot to enter the IP address you wanted to sweep provide the first three octates you want to scan"
+        echo -e "${RED}Syntax: ./scriptname.sh 192.168.0"
 else
 for ip in {0..255}; do
-ping -c 1 $1.$ip | grep "bytes from" | cut -d " " -f 4 &
+        ping -c 1 $1.$ip | grep "bytes from" | cut -d " " -f 4 &
 done
 fi
