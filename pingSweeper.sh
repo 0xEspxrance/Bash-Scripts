3dab62049dbf7f7432abd3897f27d2184badeb3f
purpler0se <78451563+purpler0se@users.noreply.github.com>
2021-05-03 20:09:58 -0400

    Update pingSweeper.sh

--- pingSweeper.sh
+++ pingSweeper.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-if [ $1 == "" ]
-then 
+if [ $1 == "" ];
+then
 echo "You forgot to enter the IP address you wanted to sweep provide the first three octates you want to scan"
 echo "Syntax ./scriptname.sh 192.18.1."
-
-else 
-for ip in `seq 1 254` : do
-ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
-done 
+else
+for ip in {0..255}; do
+ping -c 1 $1.$ip | grep "bytes from" | cut -d " " -f 4 &
+done
 fi
